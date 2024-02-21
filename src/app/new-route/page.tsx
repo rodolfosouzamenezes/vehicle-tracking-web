da041a1b5659ebe6a8bee6b3872e0b57196f5c80
rodolfosouzamenezes <rodolfosouzaofc@gmail.com>
2024-02-21 08:49:09 +0000

    feat: create route in db

--- src/app/new-route/page.tsx
+++ src/app/new-route/page.tsx
@@ -5,11 +5,17 @@ import type {
   DirectionsResponseData,
   FindPlaceFromTextResponseData,
 } from '@googlemaps/google-maps-services-js'
-import { FormEvent, useRef } from 'react'
+import { FormEvent, useRef, useState } from 'react'
+
+interface IDirectionsResponseData extends DirectionsResponseData {
+  request: any
+}
 
 export default function NewRoutePage() {
   const mapContainerRef = useRef<HTMLDivElement>(null)
   const map = useMap(mapContainerRef)
+  const [directionsData, setDirectionsData] =
+    useState<IDirectionsResponseData>()
 
   const handleSearchPlaces = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -45,9 +51,10 @@ export default function NewRoutePage() {
       `http://localhost:3000/directions?originId=${placeSourceId}&destinationId=${placeDestinationId}`,
     )
 
-    const directionsData: DirectionsResponseData & { request: any } =
+    const directionsData: IDirectionsResponseData =
       await directionsResponse.json()
 
+    setDirectionsData(directionsData)
     map?.removeAllRoutes()
 
     await map?.addRouteWithIcons({
@@ -64,6 +71,26 @@ export default function NewRoutePage() {
     })
   }
 
+  const handleCreateRoute = async () => {
+    const startAddress = directionsData!.routes[0].legs[0].start_address
+    const destinationAddress = directionsData!.routes[0].legs[0].end_address
+
+    const response = await fetch('http://localhost:3000/routes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: `${startAddress} - ${destinationAddress}`,
+        source_id: directionsData!.request.origin.place_id,
+        destination_id: directionsData!.request.destination.place_id,
+      }),
+    })
+
+    const route = await response.json()
+    console.log(route)
+  }
+
   return (
     <div className="flex flex-row h-full w-full">
       <div>
@@ -77,6 +104,15 @@ export default function NewRoutePage() {
           </div>
           <button type="submit">Pesquisar</button>
         </form>
+        {directionsData && (
+          <ul>
+            <li>Origem {directionsData.routes[0].legs[0].start_address}</li>
+            <li>Destino {directionsData.routes[0].legs[0].end_address}</li>
+            <li>
+              <button onClick={handleCreateRoute}>Criar rota</button>
+            </li>
+          </ul>
+        )}
       </div>
       <div ref={mapContainerRef} className="flex-1 bg-red-50"></div>
     </div>
